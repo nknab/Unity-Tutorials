abb159cfa55ef8a52875bdd1e99036f61c71ba01
nknab <bkojo21@gmail.com>
2020-11-23 23:41:46 +0100

    Challenge 2 Completed
    Play Fetch

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float _timeInterval = 2.0f;
+    private float _timer = 0.0f;
+   
+
+    public GameObject dogPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timer += Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && _timer > _timeInterval)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            _timer = 0.0f;
+        }
+
+    }
+}
