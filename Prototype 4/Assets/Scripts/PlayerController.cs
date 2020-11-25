1454c8690ac7b351bdc51a2cfaa984990c3f8420
nknab <bkojo21@gmail.com>
2020-11-25 15:52:30 +0100

    Lesson 4.3 Completed
    PowerUp and CountDown

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float _speed = 7.5f;
+    private float _powerUpTimer = 10.0f;
+    private float _powerUpStrength = 10f;
+
+    private Vector3 _powerUpOffset = new Vector3(0, -0.35f, 0);
+
+    private bool _hasPowerUp = false;
 
     private Rigidbody _playerRb;
+
     private GameObject _focalPoint;
+    public GameObject powerUpIndicator_;
 
     // Start is called before the first frame update
     void Start()
@@ -21,5 +29,36 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         _playerRb.AddForce(_focalPoint.transform.forward * _speed * forwardInput);
+        powerUpIndicator_.transform.position = transform.position + _powerUpOffset;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            _hasPowerUp = true;
+            powerUpIndicator_.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountDownRoutine());
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && _hasPowerUp)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            enemyRb.AddForce(awayFromPlayer * _powerUpStrength, ForceMode.Impulse);
+            Debug.Log("Player Collided with " + collision.gameObject + " with power up set to " + _hasPowerUp);
+        }
+    }
+
+    IEnumerator PowerUpCountDownRoutine()
+    {
+        yield return new WaitForSeconds(_powerUpTimer);
+        _hasPowerUp = false;
+        powerUpIndicator_.gameObject.SetActive(false);
     }
 }
