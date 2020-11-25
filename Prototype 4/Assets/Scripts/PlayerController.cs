ef270551c5c619dfedd4f04d11d6f4767ddabbb8
nknab <bkojo21@gmail.com>
2020-11-25 14:12:11 +0100

    Lesson 4.1 Completed
    Watch Where You’re Going

---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float _speed = 5.0f;
+
+    private Rigidbody _playerRb;
+    private GameObject _focalPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRb = GetComponent<Rigidbody>();
+        _focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        _playerRb.AddForce(_focalPoint.transform.forward * _speed * forwardInput);
+    }
+}
---
+++ Prototype 4/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    private float _rotationSpeed = 50.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * _rotationSpeed * Time.deltaTime);
+        
+    }
+}
