f36c9c9d4c46d6e37123395ff16d1b2d6b77b297
nknab <bkojo21@gmail.com>
2020-11-26 09:50:57 +0100

    Lesson 5.1 Completed
    Clicky Mouse

---
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private float _spawnRate = 1.0f;
+
+    public List<GameObject> targets_;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_spawnRate);
+            int index = Random.Range(0, targets_.Count);
+            Instantiate(targets_[index]);
+        }
+    }
+}
---
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private float _minSpeed = 12.0f;
+    private float _maxSpeed = 16.0f;
+    private float _maxTorque = 10.0f;
+    private float _xRange = 4.0f;
+    private float _ySpawnPos = -2.0f;
+
+    private Rigidbody _targetRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _targetRb = GetComponent<Rigidbody>();
+        _targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        _targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(_minSpeed, _maxSpeed);
+    }
+
+    private float RandomTorque()
+    {
+        return Random.Range(-_maxTorque, _maxTorque);
+    }
+
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-_xRange, _xRange), _ySpawnPos);
+    }
+}
